e74264d2cfe04a5ded2cdd9b0c08f2bf2c778d1c
Théo Grassien <theo.grassien@gmail.com>
2022-10-13 11:15:09 +0200

    Filter selection animation

--- script.js
+++ script.js
@@ -1,98 +1,76 @@
-// const data = d3.json("data/data.json").then(response);
-
-// function response(l) {
-//   let duration = 250;
-//   let n = 10;
-//   console.log(l);
-// }
-
-let filter = "TV";
-
-async function init() {
-  const response = await fetch("data/data.json");
-  const data0 = await response.json();
-
-  // Grouper le tableau par semaine
-  const data = d3.groups(data0, (d) => d.week).reverse();
-
-  let weekcounter = 1;
-  for (let week of data) {
-    function charts() {
-      // Appliquer le filtre
-      let weekFiltered = week[1].filter((w) => w.category == "TV");
-      console.log(weekFiltered);
-    }
-
-    charts();
-
-    // setTimeout(charts, weekcounter * 1000);
-    // weekcounter++;
-  }
-}
-
 const data = {
-  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-  datasets: [{
-    label: 'Weekly Sales',
-    data: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+  labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+  datasets: [
+    {
+      label: "Weekly Sales",
+      data: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 
-    backgroundColor: [
-      'red',
-      'red',
-      'red',
-      'red',
-      'red',
-      'red',
-      'red',
-      'red',
-      'red',
-      'red',
-
-    ],
-
-  }]
+      backgroundColor: [
+        "red",
+        "red",
+        "red",
+        "red",
+        "red",
+        "red",
+        "red",
+        "red",
+        "red",
+        "red",
+      ],
+    },
+  ],
 };
 
-// config 
+// config
 const config = {
-  type: 'bar',
+  type: "bar",
   data,
   options: {
-    indexAxis: 'y',
+    indexAxis: "y",
 
     scales: {
       y: {
         beginAtZero: true,
         ticks: {
           font: {
-            size: 20
-
-          }
-
-        }
-
-      }
-    }
-  }
-
-
-
+            size: 20,
+          },
+        },
+      },
+    },
+  },
 };
 
 // render init block
-const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-);
+const myChart = new Chart(document.getElementById("myChart"), config);
 
+// Filter selection animation
 
+const filmFilter = document.querySelector(".film-filter");
+const serieFilter = document.querySelector(".serie-filter");
+const tabIndicator = document.querySelector(".tab-indicator");
 
+const filmFilterWidth = filmFilter.offsetWidth;
+const serieFilterWidth = serieFilter.offsetWidth;
 
+tabIndicator.style.width = serieFilterWidth + "px";
 
+serieFilter.addEventListener("click", () => {
+  filter = "TV";
 
+  serieFilter.classList.add("selected");
+  filmFilter.classList.remove("selected");
 
+  tabIndicator.style.left = "0px";
+  tabIndicator.style.width = serieFilterWidth + "px";
+});
 
+filmFilter.addEventListener("click", () => {
+  filter = "Films";
 
+  filmFilter.classList.add("selected");
+  serieFilter.classList.remove("selected");
 
-
-init();
+  tabIndicator.style.left = serieFilterWidth + 32 + "px";
+  tabIndicator.style.width = filmFilterWidth + "px";
+});
