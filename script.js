96acc7a3be6d9e53b37b252b86c960dbd0bc6b4f
Mohand-Zenati <zenatimohandd@gmail.com>
2022-10-13 10:54:32 +0200

    ChartsJS

--- script.js
+++ script.js
@@ -30,4 +30,69 @@ async function init() {
   }
 }
 
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+  datasets: [{
+    label: 'Weekly Sales',
+    data: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+
+    backgroundColor: [
+      'red',
+      'red',
+      'red',
+      'red',
+      'red',
+      'red',
+      'red',
+      'red',
+      'red',
+      'red',
+
+    ],
+
+  }]
+};
+
+// config 
+const config = {
+  type: 'bar',
+  data,
+  options: {
+    indexAxis: 'y',
+
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          font: {
+            size: 20
+
+          }
+
+        }
+
+      }
+    }
+  }
+
+
+
+};
+
+// render init block
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
+
+
+
+
+
+
+
+
+
+
+
 init();
